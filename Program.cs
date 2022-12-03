291253c0c2fc2448928d00ffe21ca62403d69191
“akashmane3938” <maneakash3938@gmail.com>
2022-12-03 11:24:22 -0800

    Logical Programming

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Logical_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LP1_Fs.Fibonacci_Series();
+            Console.ReadLine();
+
+            LP2_PerfectNumber.Perfect();
+            Console.ReadLine();
+
+            LP3_PrimeNumber.Prime();
+            Console.ReadLine();
+
+            LP4_ReverseNumber.Reverse();
+            Console.ReadLine();
+
+            LP6_StopWatch.watch();
+            Console.ReadLine();
+        }
+    }
+}
